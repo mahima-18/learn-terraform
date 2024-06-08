ca6a8f1486cbf48a6e018c8af69ba09e971e0a98
Mahima Sharma <mahimasharma@mahimasharma-q4p73qmxq4.lan>
2024-06-08 19:32:37 +0530

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "mahi18-terraform"
 
     workspaces {
       name = "learn-terraform"
     }
   }
+*/
 
   required_providers {
     aws = {
